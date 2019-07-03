2885d9a789995522cf5c6748899e7934d1a2477b
Yizhou Shan <ys@purdue.edu>
2019-07-03 16:45:10 -0400

    pgadvance: basic threads/buffer/queue setup

--- xing/kern_mod/pgadvance.c
+++ xing/kern_mod/pgadvance.c
@@ -17,8 +17,8 @@
 #include "pgadvance.h"
 #include "../config.h"
 
-struct pgadvance_set *pas __percpu;
-struct task_struct **pgadvancers __percpu;
+DEFINE_PER_CPU(struct pgadvance_set, pas);
+DEFINE_PER_CPU(struct task_struct *, pgadvancers);
 
 static void refill_list(struct pgadvance_list *list, int cpu,
 			enum pgadvance_list_type type,
@@ -28,8 +28,8 @@ static void refill_list(struct pgadvance_list *list, int cpu,
 	struct page *page;
 	gfp_t flags = GFP_KERNEL;
 
-	pr_info("%s(): %d-%s %#lx nr:%d cpu:%d\n",
-		__func__, current->pid, current->comm, (unsigned long)list, nr_to_fill, cpu);
+	//pr_info("%s(): %d-%s %#lx nr:%d cpu:%d\n",
+	//	__func__, current->pid, current->comm, (unsigned long)list, nr_to_fill, cpu);
 
 	if (type == PGADVANCE_TYPE_ZERO)
 		flags |= __GFP_ZERO;
@@ -47,8 +47,7 @@ static void refill_list(struct pgadvance_list *list, int cpu,
 
 static struct page *cb_alloc_zero_page(void)
 {
-#if 0
-	struct pgadvance_set *p = this_cpu(pas);
+	struct pgadvance_set *p = this_cpu_ptr(&pas);
 	struct pgadvance_list *l = &p->lists[PGADVANCE_TYPE_ZERO];
 	struct page *page;
 
@@ -59,25 +58,11 @@ static struct page *cb_alloc_zero_page(void)
 	list_del(&page->lru);
 	l->count--;
 	return page;
-#endif
-	return alloc_page(__GFP_ZERO);
 }
 
 struct pgadvance_callbacks pcb = {
 	.alloc_zero_page = cb_alloc_zero_page
 };
-
-/* Free all pages of a given list */
-static void free_list(struct pgadvance_list *list)
-{
-	struct page *page;
-
-	while (!list_empty(&list->head)) {
-		page = list_first_entry(&list->head, struct page, lru);
-		list_del(&page->lru);
-		put_page(page);
-	}
-}
 
 static int cal_watermark(void)
 {
@@ -90,60 +75,8 @@ static int cal_batch(void)
 }
 
 /*
- * Allocate percpu data structure and alloc pages
- * to fill all the lists.
+ * The background cashier.
  */
-static int init_percpu_sets(void)
-{
-	enum pgadvance_list_type type;
-	int cpu, watermark, batch;
-
-	pas = alloc_percpu_gfp(struct pgadvance_set, __GFP_ZERO);
-	if (!pas)
-		return -ENOMEM;
-
-	watermark = cal_watermark();
-	batch = cal_batch();
-
-	for_each_possible_cpu(cpu) {
-		struct pgadvance_set *p = per_cpu_ptr(pas, cpu);
-		struct pgadvance_list *l;
-
-		pr_crit("%s(): %#lx\n", __func__, p);
-		for (type = 0; type < NR_PGADVANCE_TYPES; type++) {
-			l = &p->lists[type];
-
-			l->count = 0;
-			l->watermark = watermark;
-			l->batch = batch;
-			INIT_LIST_HEAD(&l->head);
-			refill_list(l, cpu, type, watermark);
-		}
-	}
-	return 0;
-}
-
-static void free_percpu_sets(void)
-{
-	int cpu;
-	enum pgadvance_list_type type;
-
-	if (!pas)
-		return;
-
-	for_each_possible_cpu(cpu) {
-		struct pgadvance_set *p = per_cpu_ptr(pas, cpu);
-		struct pgadvance_list *l;
-
-		pr_crit("%s(): %#lx\n", __func__, p);
-		for (type = 0; type < NR_PGADVANCE_TYPES; type++) {
-			l = &p->lists[type];
-			free_list(l);
-		}
-	}
-	free_percpu(pas);
-}
-
 static int pgadvancers_func(void *_unused)
 {
 	pr_crit("%s CPU %d run\n", current->comm, smp_processor_id());
@@ -161,11 +94,8 @@ static void exit_pgadvance_threads(void)
 {
 	int cpu;
 
-	if (!pgadvancers)
-		return;
-
 	for_each_possible_cpu(cpu) {
-		struct task_struct *tsk = per_cpu_ptr(pgadvancers, cpu);
+		struct task_struct *tsk = per_cpu(pgadvancers, cpu);
 		if (!IS_ERR_OR_NULL(tsk))
 			kthread_stop(tsk);
 	}
@@ -177,10 +107,6 @@ static int init_pgadvance_threads(void)
 	int cpu;
 	struct task_struct *tsk;
 
-	pgadvancers = alloc_percpu_gfp(struct task_struct *, __GFP_ZERO);
-	if (!pgadvancers)
-		return -ENOMEM;
-
 	for_each_possible_cpu(cpu) {
 		tsk = kthread_create(pgadvancers_func, NULL, "pgadvancers%d", cpu);
 		if (IS_ERR(tsk)) {
@@ -191,6 +117,63 @@ static int init_pgadvance_threads(void)
 		wake_up_process(tsk);
 
 		per_cpu(pgadvancers, cpu) = tsk;
+	}
+	return 0;
+}
+
+/* Free all pages of a given list */
+static void free_list(struct pgadvance_list *list)
+{
+	struct page *page;
+
+	while (!list_empty(&list->head)) {
+		page = list_first_entry(&list->head, struct page, lru);
+		list_del(&page->lru);
+		put_page(page);
+	}
+}
+
+static void free_percpu_sets(void)
+{
+	int cpu;
+	enum pgadvance_list_type type;
+
+	for_each_possible_cpu(cpu) {
+		struct pgadvance_set *p = per_cpu_ptr(&pas, cpu);
+		struct pgadvance_list *l;
+
+		for (type = 0; type < NR_PGADVANCE_TYPES; type++) {
+			l = &p->lists[type];
+			free_list(l);
+		}
+	}
+}
+
+/*
+ * Allocate percpu data structure and alloc pages
+ * to fill all the lists.
+ */
+static int init_percpu_sets(void)
+{
+	enum pgadvance_list_type type;
+	int cpu, watermark, batch;
+
+	watermark = cal_watermark();
+	batch = cal_batch();
+
+	for_each_possible_cpu(cpu) {
+		struct pgadvance_set *p = per_cpu_ptr(&pas, cpu);
+		struct pgadvance_list *l;
+
+		for (type = 0; type < NR_PGADVANCE_TYPES; type++) {
+			l = &p->lists[type];
+
+			l->count = 0;
+			l->watermark = watermark;
+			l->batch = batch;
+			INIT_LIST_HEAD(&l->head);
+			refill_list(l, cpu, type, watermark);
+		}
 	}
 	return 0;
 }
